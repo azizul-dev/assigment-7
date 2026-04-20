c34a17c5347ca7f6d3e6a409305daf9bbebaf441
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-04-20 19:49:21 +0600

    update profile picture url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        // ✅ যেকোনো domain থেকে image load হবে
+        
         remotePatterns: [
             {
                 protocol: "https",
